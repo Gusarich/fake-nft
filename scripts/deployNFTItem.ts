c0a801990ba5d5e718839c940a3cdb69f0b20f4c
Gusarich <danielsedovzzz@gmail.com>
2023-04-25 11:59:55 +0300

    rework contract. optimize fees.

--- scripts/deployNFTItem.ts
+++ scripts/deployNFTItem.ts
@@ -1,23 +1,25 @@
-import { Address, beginCell, toNano } from 'ton-core';
+import { Address, Cell, beginCell, toNano } from 'ton-core';
 import { NFTItem } from '../wrappers/NFTItem';
-import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
+    const code = Cell.fromBase64('te6cckEBBAEAJAABBojtVQECCK1N4I4CAwAAACQw7UTQf3CLEIAD+kDUMBA0ECNa4R1d');
+
     const nft = provider.open(
         NFTItem.createFromConfig(
             {
                 owner: Address.parse('EQBIhPuWmjT7fP-VomuTWseE8JNWv2q7QYfsVQ1IZwnMk8wL'),
-                index: 0n,
+                index: 1n,
                 content: beginCell()
                     .storeUint(1, 8)
                     .storeStringTail('https://raw.githubusercontent.com/Gusarich/fake-nft/main/scripts/metadata.json')
                     .endCell(),
             },
-            await compile('NFTItem')
+            code
         )
     );
 
-    await nft.sendDeploy(provider.sender(), toNano('0.0007'));
+    await nft.sendDeploy(provider.sender(), toNano('0.0001'));
 
     await provider.waitForDeploy(nft.address);
 }
